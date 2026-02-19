359e1fec012b62e0f42c278f073c523178b4bef1
Omkar-shrivastava <154906835+Omkar-shrivastava@users.noreply.github.com>
2026-02-19 13:08:36 +0530

    Update filter_bag_app.py

--- filter_bag_app.py
+++ filter_bag_app.py
@@ -1915,4 +1915,6 @@ if __name__ == '__main__':
     print("=" * 60)
     print("\n")
     
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
